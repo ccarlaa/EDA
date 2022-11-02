e4a5357e4f99c4b790af2bdfe9c3ed341fb0116b
Carla A. C. Ribeiro <carlacarlaclementino@gmail.com>
2022-11-02 17:11:50 -0300

    second question

---
+++ lista_1/soma_2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    int first = 0;
+    int second = 0;
+
+    scanf("%i", &first);
+    scanf("%i", &second);
+
+    int sum = first + second;
+
+    printf("%d\n", sum);
+
+}
